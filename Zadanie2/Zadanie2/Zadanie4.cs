7fd22da0446e738322106dfee15e98a7620d882d
Mateusz-Rudas <s27052@pjwstk.edu.pl>
2025-03-12 20:54:28 +0100

    Dodano klase Zadanie4

---
+++ Zadanie2/Zadanie2/Zadanie4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Zadanie4
+{
+    public static int FindMaxValue(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta");
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+                max = num;
+        }
+        return max;
+    }
+}
+
