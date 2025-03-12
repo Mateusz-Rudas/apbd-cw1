9c033854a25a40ee3ae52e84e8db33e23f178ea0
Mateusz-Rudas <s27052@pjwstk.edu.pl>
2025-03-12 20:35:21 +0100

    Dodano klase Calculator

---
+++ Zadanie2/Zadanie2/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Calculator
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta");
+
+        return numbers.Average();
+    }
+}
